635cf23cd92b5b8c4495c44b7fda280ccdcfa970
freeloki <yavuzerzurumlu@gmail.com>
2016-06-03 11:25:52 +0300

    Add broadcast receiver and tests.

--- libraries/connectivity/libwifi/app/src/androidTest/java/com/ardic/android/connectivity/libwirelessconnection/ApplicationTest.java
+++ libraries/connectivity/libwifi/app/src/androidTest/java/com/ardic/android/connectivity/libwirelessconnection/ApplicationTest.java
@@ -1,13 +1,153 @@
 package com.ardic.android.connectivity.libwirelessconnection;
 
 import android.app.Application;
+import android.app.Instrumentation;
+import android.net.NetworkInfo;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
+import android.test.InstrumentationTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import com.ardic.android.connectivity.libwirelessconnection.listeners.DetailedWifiConnectionListener;
+import com.ardic.android.connectivity.libwirelessconnection.listeners.SimpleWifiConnectionListener;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import dalvik.annotation.TestTargetClass;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public class ApplicationTest extends InstrumentationTestCase{
+
+    private WifiHelper mWifiHelperSimple;
+    private WifiHelper mWifiHelperDetailed;
+
+    private SimpleWifiConnectionListener listener = new SimpleWifiConnectionListener() {
+        @Override
+        public void onConnecting(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onConnected(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onDisconnected(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onDisconnecting(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onSuspended(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onUnknown(NetworkInfo info) {
+
+        }
+    };
+
+    private DetailedWifiConnectionListener detailedListener = new DetailedWifiConnectionListener() {
+        @Override
+        public void onAuthenticating(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onBlocked(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onCaptivePortalCheck(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onConnected(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onConnecting(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onDisconnecting(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onDisconnected(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onFailed(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onIdle(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onObtainingIpAddr(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onScanning(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onSuspended(NetworkInfo info) {
+
+        }
+
+        @Override
+        public void onVerifyingPoorLink(NetworkInfo info) {
+
+        }
+    };
+
+    @Before
+    public void createWifiHelper() {
+       mWifiHelperSimple = WifiHelper.WifiHelperFactory.create(InstrumentationRegistry.getContext(),listener);
+        mWifiHelperDetailed = WifiHelper.WifiHelperFactory.create(InstrumentationRegistry.getContext(),detailedListener);
     }
+    @Test
+    public void WifiHelperSimpleListenerTest(){
+
+        // simple
+        assertNotNull(mWifiHelperSimple);
+        assertNotNull(mWifiHelperSimple.getWifiManager());
+        assertNull(mWifiHelperSimple.getDetailedWifiConnectionListener());
+    }
+
+    @Test
+    public void WifiHelperDetailedListenerTest(){
+
+        //detailed
+        assertNotNull(mWifiHelperDetailed);
+        assertNotNull(mWifiHelperDetailed.getWifiManager());
+        assertNull(mWifiHelperSimple.getSimpleWifiConnectionListener());
+    }
+
 }
--- libraries/connectivity/libwifi/app/src/main/java/com/ardic/android/connectivity/libwirelessconnection/WifiHelper.java
+++ libraries/connectivity/libwifi/app/src/main/java/com/ardic/android/connectivity/libwirelessconnection/WifiHelper.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 
+import com.ardic.android.connectivity.libwirelessconnection.listeners.DetailedWifiConnectionListener;
+import com.ardic.android.connectivity.libwirelessconnection.listeners.SimpleWifiConnectionListener;
+
 import java.util.List;
 
 /**
@@ -13,20 +16,59 @@ public class WifiHelper {
 
     private Context appContext;
     private WifiManager mWifiManager;
+    private WifiStateReceiver mWifiStateReceiver;
+    private DetailedWifiConnectionListener mDetailedWifiConnectionListener;
+    private SimpleWifiConnectionListener mSimpleWifiConnectionListener;
 
-    private WifiHelper(Context appContext){
+    private WifiHelper(Context appContext, SimpleWifiConnectionListener listener) {
         mWifiManager = (WifiManager) appContext.getSystemService(Context.WIFI_SERVICE);
-   }
+        mWifiStateReceiver = new WifiStateReceiver();
+        if (listener != null) {
+            mWifiStateReceiver.setSimpleWifiConnectionListener(listener);
+        }
+    }
 
-    public boolean isWifiEnabled(){
+    private WifiHelper(Context appContext, DetailedWifiConnectionListener listener) {
+        mWifiManager = (WifiManager) appContext.getSystemService(Context.WIFI_SERVICE);
+        mWifiStateReceiver = new WifiStateReceiver();
+        if (listener != null) {
+            mWifiStateReceiver.setDetailedWifiConnectionListener(listener);
+        }
+    }
+
+    public WifiManager getWifiManager() {
+        return this.mWifiManager;
+    }
+
+    public DetailedWifiConnectionListener getDetailedWifiConnectionListener() {
+        return this.mDetailedWifiConnectionListener;
+    }
+
+    public void setDetailedWifiConnectionListener(DetailedWifiConnectionListener listener) {
+        if (listener != null) {
+            this.mDetailedWifiConnectionListener = listener;
+        }
+    }
+
+    public SimpleWifiConnectionListener getSimpleWifiConnectionListener() {
+        return this.mSimpleWifiConnectionListener;
+    }
+
+    public void setSimpleWifiConnectionListener(SimpleWifiConnectionListener listener) {
+        if (listener != null) {
+            this.mSimpleWifiConnectionListener = listener;
+        }
+    }
+
+    public boolean isWifiEnabled() {
         return mWifiManager.isWifiEnabled();
     }
 
-    public boolean setWifiEnabled(boolean enabled){
+    public boolean setWifiEnabled(boolean enabled) {
         return mWifiManager.setWifiEnabled(enabled);
     }
 
-    public boolean connectToWEPNetwork(String ssid, String password, boolean isHidden){
+    public boolean connectToWEPNetwork(String ssid, String password, boolean isHidden) {
 
         WifiConfiguration mWifiConf = new WifiConfiguration();
         mWifiConf.hiddenSSID = isHidden;
@@ -37,20 +79,20 @@ public class WifiHelper {
         mWifiConf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
 
         mWifiManager.addNetwork(mWifiConf);
-        return  connectToConfiguredSSID(ssid);
+        return connectToConfiguredSSID(ssid);
     }
 
-    public boolean connectToWPANetwork(String ssid,String password, boolean isHidden){
+    public boolean connectToWPANetwork(String ssid, String password, boolean isHidden) {
         WifiConfiguration mWifiConf = new WifiConfiguration();
         mWifiConf.hiddenSSID = isHidden;
         mWifiConf.SSID = ssid;
-        mWifiConf.preSharedKey = "\""+ password +"\"";
+        mWifiConf.preSharedKey = "\"" + password + "\"";
 
         mWifiManager.addNetwork(mWifiConf);
         return connectToConfiguredSSID(ssid);
     }
 
-    public boolean connectToOpenNetwork(String ssid,String password, boolean isHidden){
+    public boolean connectToOpenNetwork(String ssid, String password, boolean isHidden) {
         WifiConfiguration mWifiConf = new WifiConfiguration();
         mWifiConf.hiddenSSID = isHidden;
         mWifiConf.SSID = ssid;
@@ -59,10 +101,10 @@ public class WifiHelper {
         return connectToConfiguredSSID(ssid);
     }
 
-    private boolean connectToConfiguredSSID(String ssid){
+    private boolean connectToConfiguredSSID(String ssid) {
         List<WifiConfiguration> list = mWifiManager.getConfiguredNetworks();
-        for( WifiConfiguration i : list ) {
-            if(i.SSID != null && i.SSID.equals("\"" + ssid + "\"")) {
+        for (WifiConfiguration i : list) {
+            if (i.SSID != null && i.SSID.equals("\"" + ssid + "\"")) {
                 mWifiManager.disconnect();
                 mWifiManager.enableNetwork(i.networkId, true);
                 return mWifiManager.reconnect();
@@ -71,20 +113,33 @@ public class WifiHelper {
         return false;
     }
 
-    public boolean disconnect(){
-       return  mWifiManager.disconnect();
+    public boolean disconnect() {
+        return mWifiManager.disconnect();
     }
+
     /**
      * Factory class for create instance for WifiHelper
      */
     public static class WifiHelperFactory {
 
-        public static WifiHelper create(Context appContext) throws IllegalArgumentException{
-            if(appContext!=null) {
-                return  new WifiHelper(appContext);
-            }else{
+        public static WifiHelper create(Context appContext, SimpleWifiConnectionListener listener) throws IllegalArgumentException {
+            if (appContext != null) {
+                return new WifiHelper(appContext, listener);
+            } else {
                 throw new IllegalArgumentException("Application Context is NULL.");
             }
-        };
+        }
+
+        ;
+
+        public static WifiHelper create(Context appContext, DetailedWifiConnectionListener listener) throws IllegalArgumentException {
+            if (appContext != null) {
+                return new WifiHelper(appContext, listener);
+            } else {
+                throw new IllegalArgumentException("Application Context is NULL.");
+            }
+        }
+
+        ;
     }
 }
