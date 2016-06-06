3b40c87b952e5ba6e84c73efce7b6b76df05f531
freeloki <yavuzerzurumlu@gmail.com>
2016-06-06 15:32:06 +0300

    Add libnsd.

---
+++ libraries/connectivity/libnsd/nsdhelper/src/main/java/com/ardic/android/libnsd/nsdhelper/CommonNSDServiceTypes.java
@@ -0,0 +1,14 @@
+
+package com.ardic.android.libnsd.nsdhelper;
+
+public class CommonNSDServiceTypes {
+
+    public static final String EDISON_SERVICE = "_xdk-app-daemon._tcp";
+    public static final String WORKSTATION_SERVICE = "_workstation._tcp";
+    public static final String HTTP_SERVICE = "_http._tcp";
+    public static final String PRINTER_SERVICE = "_ipp._tcp";
+    public static final String ARDUINO_SERVICE = "_arduino._tcp";
+    public static final String AXIS_CAMERA_SERVICE = "_axis-video._tcp";
+    public static final String SPEEDWAY_RF_ID_SERVICE = "_llrp._tcp";
+
+}
