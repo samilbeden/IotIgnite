d9908e5c175c16ff7959a961f5c7dfb2cd9541bf
freeloki <yavuzerzurumlu@gmail.com>
2016-06-06 14:35:11 +0300

    Add libbluetooth to SampleProject as module.

---
+++ SampleIotIgniteProject/bluetooth/src/main/java/com/ardic/android/libbluetoothconnection/interfaces/MessagingInterface.java
@@ -0,0 +1,10 @@
+package com.ardic.android.libbluetoothconnection.interfaces;
+
+import android.os.Message;
+
+/**
+ * Created by Yavuz on 6.06.2016.
+ */
+public interface MessagingInterface {
+    void onMessageReceived(Message msg);
+}
